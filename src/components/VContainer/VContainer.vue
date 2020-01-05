b8b2ec825674ae70f8400e75137ecd2aeb5d0cef
Vinicius Lana <contato@viniciuslana.com>
2020-01-05 19:46:11 -0300

    Create container component

---
+++ src/components/VContainer/VContainer.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  div.v-container
+    div.v-container__header
+      slot(name='header')
+    div.v-container__body
+      slot
+</template>
+
+<style lang="scss" src="./VContainer.scss" scoped></style>
