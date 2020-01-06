dc61c9e35e5cd3c7814abb6954b08e50eb73b8a3
Vinicius Lana <contato@viniciuslana.com>
2020-01-06 08:08:20 -0300

    Add Card and company resume base components

---
+++ src/components/VCard/VCard.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+  .v-card
+    .v-card__header
+      slot(name='header')
+    .v-card__body
+      slot
+</template>
--- src/pages/Company/CompanyPage.vue
+++ src/pages/Company/CompanyPage.vue
@@ -1,4 +1,19 @@
 <template lang="pug">
+  div
+    v-company-resume
+    v-card
+    v-card
+    v-card
 </template>
 
 <script>
+import VCard from 'Components/VCard/VCard.vue'
+import VCompanyResume from 'Components/VCompanyResume/VCompanyResume.vue'
+
+export default {
+  components: {
+    VCard,
+    VCompanyResume
+  }
+}
+</script>
