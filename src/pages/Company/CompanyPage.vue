8c5cd4612503061e2ca28d794b79fc2b4d8c5e44
Vinicius Lana <contato@viniciuslana.com>
2020-01-06 08:32:35 -0300

    Mock cards

--- src/pages/Company/CompanyPage.vue
+++ src/pages/Company/CompanyPage.vue
@@ -2,8 +2,17 @@
   div
     v-company-resume
     v-card
+      template(slot='header') Activities
+      template Body
     v-card
+      template(slot='header') Similar Companies
+      template Body
     v-card
+      template(slot='header') Company Spend History
+      template Body
+    v-card
+      template(slot='header') Company Spend History
+      template Body
 </template>
 
 <script>
